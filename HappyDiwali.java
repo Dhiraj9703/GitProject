409f33a0210138e6fedb42df73bdbaf48adb657b
Dheeraj patil <dhirajpatil769@gmail.com>
2023-03-16 21:02:00 +0530

    file added

--- HappyDiwali.java
+++
@@ -1,9 +0,0 @@
-package javavariablesdemo;
-
-public class HappyDiwali {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
- System.out.println("Happy diwali to all");	}
-
-}
